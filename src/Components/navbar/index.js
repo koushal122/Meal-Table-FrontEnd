7d6fbe0387ef4455f18f53dbfdd3e820435b37c7
Koushal Jha <koushaljha887@gmail.com>
2023-10-29 19:20:02 +0530

    initialized project and added carousel and navbar

---
+++ src/Components/navbar/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './index.scss';
+import cart from '../../Images/cart-icon.png';
+
+const Header = () => {
+  return (
+    <div className="navbar">
+       <div id="logo">
+            <div>COFFEE</div>
+            <small>BLEND</small>
+       </div>
+       <ul className="navbar-list">
+        <li className="navbar-list-item">HOME</li>
+        <li className="navbar-list-item" >MENU</li>
+        <li className="navbar-list-item" >SERVICES</li>
+        <li className="navbar-list-item" >BLOG</li>
+        <li className="navbar-list-item" >ABOUT</li>
+        <li className="navbar-list-item" >SHOP</li>
+        <li className="navbar-list-item" >CONTACT</li>
+        <img src={cart} alt='cart' className="navbar-list-item" style={{width:'20px'}} />
+       </ul>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Constants/constants.js
@@ -0,0 +1,24 @@
+import bg1 from '../Images/carousel_bg_1.png';
+import bg2 from '../Images/carousel_bg_2.png';
+import bg3 from '../Images/carousel_bg3.png';
+
+ export const CAROUSEL_SLIDERS=[
+    {
+        id:1,
+        src:bg1,
+        title:'THE BEST COFFEE TESTING EXPERIENCE',
+        alt:'background image 1',
+    },
+    {
+        id:2,
+        src:bg2,
+        title:'CREAMY HOT AND READY TO SERVE',
+        alt:'background image 2',
+    },
+    {
+        id:3,
+        src:bg3,
+        title:'AMAZING TASTE AND BEAUTIFUL PLACE',
+        alt:'background image 3',
+    }
+]
